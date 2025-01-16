eb9a713a4baac2b031a394ae8cbe0331da8a800e
tilahun <tilahunbogale@gmail.com>
2025-01-16 18:44:51 -0500

    dfff

---
+++ src/main/java/com/springmvc2/demomvc/LoginController.java
@@ -0,0 +1,40 @@
+package com.springmvc2.demomvc;
+
+// Controller layer for Login Page
+// Using Model
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// A simple controller using
+// spring web annotations
+@Controller
+public class LoginController {
+
+    // returning the login jsp
+    // for root of the webpage
+    @GetMapping("/")
+    public String home(){
+        return "login.jsp";
+    }
+
+    // mapping the "/login" request
+    // requestparam is used to get the value
+    // from get request parameters
+    @GetMapping("/login")
+    public String loginActionUsingModel(
+            @RequestParam("name") String name,
+            @RequestParam("password") String pass, Model model
+    ){
+
+        // adding the attribute passed from request
+        // to the model object
+        model.addAttribute("userName", name);
+        model.addAttribute("password", pass);
+
+        // returning the view
+        return "templates/result.jsp";
+    }
+}
